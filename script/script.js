a159d10b6651dc8f527221e1736dc6d1918d99c7
AkosBartus <bartusakos0@gmail.com>
2023-06-28 11:52:00 +0200

    :)

--- script/script.js
+++ script/script.js
@@ -1,8 +1,4 @@
 "use strict";
-/* import { API_KEY } from './config';
-
-const url = `http://api.weatherapi.com/v1?apiKey=${API_KEY}`;
- */
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -12,50 +8,34 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-let load = () => __awaiter(void 0, void 0, void 0, function* () {
-    let response = yield fetch("http://api.weatherapi.com/v1/forecast.json?key=04e4c702cf3b4efc921170534232706&q=New York&days=3&aqi=no&alerts=no");
-    let data = yield response.json();
+const apiKey = 'a3e3462b47bbc9a7e2cb32ec5dff6423';
+const loadWeatherData = (city) => __awaiter(void 0, void 0, void 0, function* () {
+    const response = yield fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+    const data = yield response.json();
+    allCitiesArray.push(data.name);
     return data;
 });
-load();
-let load2 = () => __awaiter(void 0, void 0, void 0, function* () {
-    let response = yield fetch("http://api.weatherapi.com/v1/forecast.json");
-    let data = yield response.json();
-    return data;
-});
-load2();
-let load3 = () => __awaiter(void 0, void 0, void 0, function* () {
-    let response = yield fetch("http://api.weatherapi.com/v1/history.json");
-    let data = yield response.json();
-    return data;
-});
-load3();
-const dataArray = [
-    'Budapest',
-    'Miskolc',
-    'Salgótarján',
-    'Mikolc',
-    'Nyíregyháza',
-    'Debrecen',
-    'Békéscsaba',
-    'Szolnok',
-    'Eger',
-    'Szeged',
-    'Kecskemét',
-    'Tatabánya',
-    'Székesfehérvár',
-    'Szekszárd',
-    'Pécs',
-    'Győr',
-    'Veszprém',
-    'Kaposvár',
-    'Zalaegerszeg',
-    'Szombathely'
-];
+let city = '';
+let allCitiesArray = [];
 let userInput;
-const inputBox = document.querySelector('.inp');
-const suggestion = document.querySelector('.suggestionBox');
+const inputBox = document.getElementById('input-box');
+const suggestion = document.getElementById('suggestion-box');
 let filteredArray = [];
+const displayErrorMessage = (error) => {
+    const resultParagraphElement = document.getElementById('result-paragraph');
+    if (resultParagraphElement) {
+        resultParagraphElement.textContent = `Hiba történt: ${error.message}`;
+    }
+};
+const updateWeatherData = (city) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const data = yield loadWeatherData(city);
+        displayWeatherData(data);
+    }
+    catch (error) {
+        displayErrorMessage(error.message);
+    }
+});
 const getInput = (event) => {
     userInput = event.target.value;
     if (userInput === '') {
@@ -66,9 +46,31 @@ const getInput = (event) => {
         filterArray();
         renderSuggestions();
     }
+    if (event instanceof KeyboardEvent && event.key === 'Enter') {
+        selectCity(event);
+    }
 };
+const updateWeatherDataForCities = (cities) => __awaiter(void 0, void 0, void 0, function* () {
+    for (const city of cities) {
+        yield updateWeatherData(city);
+    }
+});
+const cities = ['New York', 'Budapest', 'Prága', 'Pozsony', 'Párizs', 'London'];
+const selectCity = (event) => {
+    const selectedCity = event.target.textContent;
+    if (selectedCity) {
+        userInput = selectedCity;
+        inputBox.querySelector('input').value = userInput;
+        updateWeatherData(userInput);
+    }
+};
+const showMeButton = document.getElementById('show-me-button');
+showMeButton.addEventListener('click', selectCity);
+inputBox.querySelector('input').addEventListener('input', getInput);
+suggestion.addEventListener('click', selectCity);
+suggestion.addEventListener('mousedown', selectCity);
 const filterArray = () => {
-    filteredArray = dataArray.filter((item) => item.toLowerCase().includes(userInput.toLowerCase()));
+    filteredArray = allCitiesArray.filter((item) => item.toLowerCase().includes(userInput.toLowerCase()));
 };
 const renderSuggestions = () => {
     suggestion.innerHTML = '';
@@ -80,28 +82,30 @@ const renderSuggestions = () => {
         });
     }
 };
-inputBox.addEventListener('input', getInput);
-let celsius = document.getElementById('celsius');
-const pElement1 = celsius.append('19°');
-let city = document.getElementById('city');
-const pElement3 = city.append('New York');
-let statusOfWeather = document.getElementById('statusOfWeather');
-const pElement2 = statusOfWeather.append('Sunny');
-let picture = document.getElementById('pictureOfWeather');
-const svgElement = picture.append('');
-let resultParagraphElement = document.getElementById("result-paragraph");
-/*   function clock(){
-
-    let today = new Date();
-    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    let ora = String(today.getHours()).padStart(2, '0');
-    let perc = String(today.getMinutes()).padStart(2, '0');
-    let masodperc = String(today.getSeconds()).padStart(2, '0');
-    let dateTime = ora + ":" + perc  + ":" + masodperc;
-    
-        document.getElementById("ido")!.innerHTML = "A pontos idő: " + date + " " +  dateTime
-    
-    let t = setTimeout(function(){ clock() }, 1000);
-    }
-    clock();
- */
+const displayWeatherData = (data) => {
+    const celsius = document.getElementById('celsius');
+    const celsiusValue = (data.main.temp - 273.15).toFixed(2);
+    celsius.textContent = `${celsiusValue}°C`;
+    const cityx = document.getElementById('city');
+    cityx.textContent = data.name;
+    const statusOfWeather = document.getElementById('statusOfWeather');
+    statusOfWeather.textContent = data.weather[0].description;
+    const picture = document.getElementById('pictureOfWeather');
+    picture.innerHTML = `<svg ...></svg>`;
+    const realFeel = document.getElementById('real-feel');
+    const realFeelCelsius = (data.main.feels_like - 273.15).toFixed(2);
+    realFeel.textContent = `Real Feel: ${realFeelCelsius}°C`;
+    const humidity = document.getElementById('humidity');
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+    const maxMin = document.getElementById('max-min');
+    const maxCelsius = (data.main.temp_max - 273.15).toFixed(2);
+    const minCelsius = (data.main.temp_min - 273.15).toFixed(2);
+    maxMin.textContent = `Max/Min: ${maxCelsius}°C / ${minCelsius}°C`;
+    const uvIndex = document.getElementById('uv-index');
+    uvIndex.textContent = `UV-Index: ${data.uv_index}`;
+    const wind = document.getElementById('wind');
+    wind.textContent = `Wind: ${data.wind.speed} km/h`;
+    const chance = document.getElementById('chance');
+    chance.textContent = `Rain chance: ${data.rain ? data.rain["1h"] : 0}%`;
+};
+updateWeatherDataForCities(cities);
--- src/script.ts
+++ src/script.ts
@@ -1,118 +1,180 @@
-let load = async () => {
-  let response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=04e4c702cf3b4efc921170534232706&q=${city}&days=3&aqi=no&alerts=no`)
-  let data: String[] = await response.json()
+const apiKey = 'a3e3462b47bbc9a7e2cb32ec5dff6423';
 
-  return data
-}
-  
-load()
-
-let city: string
-
-/* let load2 = async () => {
-  let response = await fetch("http://api.weatherapi.com/v1/forecast.json")
-  let data: String[] = await response.json()
-  
-  return data
-}
-    
-load2()
-
-let load3 = async () => {
-  let response = await fetch("http://api.weatherapi.com/v1/history.json")
-  let data: String[] = await response.json()
-  
-  return data
-}
-    
-load3() */
-
-const dataArray: string[] = [
-    'Budapest',
-    'Miskolc',
-    'Salgótarján',
-    'Mikolc',
-    'Nyíregyháza',
-    'Debrecen',
-    'Békéscsaba',
-    'Szolnok',
-    'Eger',
-    'Szeged',
-    'Kecskemét',
-    'Tatabánya',
-    'Székesfehérvár',
-    'Szekszárd',
-    'Pécs',
-    'Győr',
-    'Veszprém',
-    'Kaposvár',
-    'Zalaegerszeg',
-    'Szombathely'
+interface WeatherData {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    }
   ];
-  
-  let userInput: string;
-  const inputBox = document.querySelector<HTMLDivElement>('.inp')!;
-  const suggestion = document.querySelector<HTMLDivElement>('.suggestionBox')!;
-  let filteredArray: string[] = [];
-  
-  const getInput = (event: Event) => {
-    userInput = (event.target as HTMLInputElement).value;
-    if (userInput === '') {
-      filteredArray = [];
-      renderSuggestions();
-    } else {
-      filterArray();
-      renderSuggestions();
-    }
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    grnd_level: number;
   };
-
-  const filterArray = () => {
-    filteredArray = dataArray.filter((item) =>
-      item.toLowerCase().includes(userInput.toLowerCase())
-    );
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
   };
-  
-  const renderSuggestions = () => {
-    suggestion.innerHTML = '';
-    if (filteredArray.length > 0) {
-      filteredArray.forEach((item) => {
-        const divElement = document.createElement('div');
-        divElement.textContent = item;
-        suggestion.appendChild(divElement);
-      });
-    }
+  clouds: {
+    all: number;
   };
-  
-  inputBox.addEventListener('input', getInput);
+  rain?: {
+    "1h": number;
+    "3h": number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+  uv_index: number;
+}
 
+const loadWeatherData = async (city: string): Promise<WeatherData> => {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  );
+  const data: WeatherData = await response.json();
+  allCitiesArray.push(data.name);
+  return data;
+};
 
-  let celsius = document.getElementById('celsius')
-  const pElement1 = (celsius as HTMLElement).append('19°')
+let city: string = '';
 
-  let cityx = document.getElementById('city')
-  const pElement3 = (cityx as HTMLElement).append('New York')
+let allCitiesArray: string[] = [];
 
-  let statusOfWeather = document.getElementById('statusOfWeather')
-  const pElement2 = (statusOfWeather as HTMLElement).append('Sunny')
+let userInput: string;
+const inputBox = document.getElementById('input-box')!;
+const suggestion = document.getElementById('suggestion-box')!;
+let filteredArray: string[] = [];
 
-  let picture = document.getElementById('pictureOfWeather')
-  const svgElement = (picture as HTMLElement).append('')
+const displayErrorMessage = (error: unknown) => {
+  const resultParagraphElement = document.getElementById('result-paragraph');
+  if (resultParagraphElement) {
+    resultParagraphElement.textContent = `Hiba történt: ${(error as Error).message}`;
+  }
+};
 
-  let resultParagraphElement = document.getElementById("result-paragraph")!
+const updateWeatherData = async (city: string) => {
+  try {
+    const data = await loadWeatherData(city);
+    displayWeatherData(data);
+  } catch (error) {
+    displayErrorMessage((error as Error).message);
+  }
+};
 
+const getInput = (event: Event) => {
+  userInput = (event.target as HTMLInputElement).value;
+  if (userInput === '') {
+    filteredArray = [];
+    renderSuggestions();
+  } else {
+    filterArray();
+    renderSuggestions();
+  }
 
-/*   function clock(){
+  if (event instanceof KeyboardEvent && event.key === 'Enter') {
+    selectCity(event);
+  }
+};
 
-    let today = new Date();
-    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    let ora = String(today.getHours()).padStart(2, '0');
-    let perc = String(today.getMinutes()).padStart(2, '0');
-    let masodperc = String(today.getSeconds()).padStart(2, '0');
-    let dateTime = ora + ":" + perc  + ":" + masodperc;
-    
-        document.getElementById("ido")!.innerHTML = "A pontos idő: " + date + " " +  dateTime
-    
-    let t = setTimeout(function(){ clock() }, 1000);
-    }
-    clock();
- */
+const updateWeatherDataForCities = async (cities: string[]) => {
+  for (const city of cities) {
+    await updateWeatherData(city);
+  }
+};
+
+const cities = ['New York', 'Budapest', 'Prága', 'Pozsony', 'Párizs', 'London'];
+
+const selectCity = (event: Event) => {
+  const selectedCity = (event.target as HTMLDivElement).textContent;
+  if (selectedCity) {
+    userInput = selectedCity;
+    inputBox.querySelector('input')!.value = userInput;
+    updateWeatherData(userInput);
+  }
+};
+
+const showMeButton = document.getElementById('show-me-button')!;
+showMeButton.addEventListener('click', selectCity);
+
+inputBox.querySelector('input')!.addEventListener('input', getInput);
+suggestion.addEventListener('click', selectCity);
+suggestion.addEventListener('mousedown', selectCity);
+const filterArray = () => {
+  filteredArray = allCitiesArray.filter((item) =>
+    item.toLowerCase().includes(userInput.toLowerCase())
+  );
+};
+
+const renderSuggestions = () => {
+  suggestion.innerHTML = '';
+  if (filteredArray.length > 0) {
+    filteredArray.forEach((item) => {
+      const divElement = document.createElement('div');
+      divElement.textContent = item;
+      suggestion.appendChild(divElement);
+    });
+  }
+};
+
+const displayWeatherData = (data: WeatherData) => {
+  const celsius = document.getElementById('celsius') as HTMLElement;
+  const celsiusValue = (data.main.temp - 273.15).toFixed(2);
+  celsius.textContent = `${celsiusValue}°C`;
+
+  const cityx = document.getElementById('city') as HTMLElement;
+  cityx.textContent = data.name;
+
+  const statusOfWeather = document.getElementById('statusOfWeather') as HTMLElement;
+  statusOfWeather.textContent = data.weather[0].description;
+
+  const picture = document.getElementById('pictureOfWeather') as HTMLElement;
+  picture.innerHTML = `<svg ...></svg>`;
+
+  const realFeel = document.getElementById('real-feel') as HTMLElement;
+  const realFeelCelsius = (data.main.feels_like - 273.15).toFixed(2);
+  realFeel.textContent = `Real Feel: ${realFeelCelsius}°C`;
+
+  const humidity = document.getElementById('humidity') as HTMLElement;
+  humidity.textContent = `Humidity: ${data.main.humidity}%`;
+
+  const maxMin = document.getElementById('max-min') as HTMLElement;
+  const maxCelsius = (data.main.temp_max - 273.15).toFixed(2);
+  const minCelsius = (data.main.temp_min - 273.15).toFixed(2)
+  maxMin.textContent = `Max/Min: ${maxCelsius}°C / ${minCelsius}°C`;
+
+  const uvIndex = document.getElementById('uv-index') as HTMLElement;
+  uvIndex.textContent = `UV-Index: ${data.uv_index}`;
+
+  const wind = document.getElementById('wind') as HTMLElement;
+  wind.textContent = `Wind: ${data.wind.speed} km/h`;
+
+  const chance = document.getElementById('chance') as HTMLElement;
+  chance.textContent = `Rain chance: ${data.rain ? data.rain["1h"] : 0}%`;
+};
+
+updateWeatherDataForCities(cities);
